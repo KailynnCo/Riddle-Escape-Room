d6d221a49fffff4175d8904e263e4191438baff8
KailynnCo <55412486+KailynnCo@users.noreply.github.com>
2021-09-27 13:02:04 -0700

    Update Riddle Escape Room.py

--- Riddle Escape Room.py
+++ Riddle Escape Room.py
@@ -1,26 +1,69 @@
-#this is my intro
-print("Wellcome to my escape room. You must guess the serect word in order to win \n ")
+import hashlib
+import string 
+from random import *
+#using def function to make my code cleaner. The def function will now carry the password function.
+def password_function():
+ print("\n \nNow you will be given a password you will need to decrypt. This password is the different every round.You will only have 1 attempt to solve this password.")
 
-#clue 
-print("Clue: There was a castastrpohic cyber attack recently. \n The governemt is still searching for the hacker. \n They think he .??? \n ")
+ characters = string.ascii_letters + string.digits + string.punctuation 
+#we use the import funtion "string" to get these
+
+ password = "".join(choice(characters) for x in range(randint(3,8)))
+ print("Testing to seperate section 3")
+ print(password)
+#ranint means random integer
+#side note: make sure everything is tabbed between the def functions or else it wont run
+#print(password) #password will not be shown in the future 
+
+def MD5_function():
+  print("\n \n Hint:MD5 Hash")
+  hash_obj = hashlib.md5(b'Hello, Python!')
+  print(hash_obj.hexdigest())
+
+#First part of program:
+
+teamname = [] #emtpy list,this will store the teams name
+
+print("Welcome to the cyber game. Please input your team name: \n")
+teamname.append(input("Team Name: ")) #.append to add input to the list
+
+print("\nWellcome", teamname) #the \n adds a space
+print("During this game you will be given a scenario based on solving encrpytion/ciphers, passwords and riddles.")
+
+#Second part of program: 
+#I looked up simple questions about the basics of what is cryptography
+print("\nWelcome to part one of the game. You have 3 attempts in order to get the correct answer. \n \nQuestion 1: What is the study of secure communications techniques that allow only the sender and intended recipient of a message to view its contents.")
 
 #assign variables 
-secret_word = "ransomeware"
-guess = ""
-guess_number = 0
-guess_limit = 3
-out_guess = False 
+part1_word = "cryptography"
+answer = ""
+answer_number = 0
+answer_limit = 3
+out = False 
 
 
-while guess != secret_word and not out_guess:
-  if guess_number < guess_limit:
-      guess = str(input("Enter your guess: "))
-      guess_number += 1
+while answer != part1_word and not out:
+  if answer_number < answer_limit:
+      answer = str(input("Enter your answer: "))
+      answer_number += 1
   else:
-    out_guess = True
+    out = True
 
-if out_guess:
+if out:
   print("oh no you loose")
 else:
-  print("Yai you win!!")
+  print("Awesome Job! Its now time to go into Question # 2")
+  
+
+ #password will not be shown in the future , P.S make sure the password_function() is within the else: and print command. Or else it will not run the program because it is no within the command
+
+#Third part of program:
+#this having the person guess the password once its encrypted
+  password_function()
+  
+#Fourth part of program:MD5 hash password 
+  MD5_function()
+
+#5th part: ask user to decrpyt the password
+
 
